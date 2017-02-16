a9ccc41a552b84c60ad6043af9e160e7d8422f7a
ColdMaster18 <riikivargas@gmail.com>
2017-02-16 19:08:03 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+    <html>
+    <head>
+        <title>Home Corpus Linguistícos</title>
+        <meta http-equiv="Content-Type" content="text/html", charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="style-inicial.css">    
+    </head>
+    <body>
+
+    	<header>
+    		<p id="logo">Corpus Linguisticos</p><br></br>
+        </header>
+
+        <nav>
+            <ul>
+                <li><a href="Index.php">Home</a></li>
+                <li><a href="Quem-somos.php">Quem Somos</a></li>
+                <li><a href="Contato.php">Contato</a></li>
+                <li><a href="Login.php">Logar</a></li>
+            </ul>
+        </nav>
+
+        <aside>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p><br></br>
+            <p>campo de texto</p>
+        </aside>
+
+    	<footer>
+            <button type="button" onclick="window.open('Rodape.php');">Rodapé 1</button>
+            <button type="button" onclick="window.open('Rodape.php');">Rodapé 2</button>
+            <button type="button" onclick="window.open('Rodape.php');">Rodapé 3</button>
+            <button type="button" onclick="window.open('Rodape.php');">Rodapé 4</button>
+            <button type="button" onclick="window.open('Rodape.php');">Rodapé 5</button>
+    	</footer>
+
+    </body>
+    </html>
