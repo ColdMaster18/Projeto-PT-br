206e86be7587270debd368e4e3d60212557c07b5
ColdMaster18 <riikivargas@gmail.com>
2017-02-16 19:09:43 -0200

    Create Redefinir.php

---
+++ Redefinir.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Login Corpus Linguistícos</title>
+        <meta http-equiv="Content-Type" content="text/html", charset="utf-8" />
+        <style type="text/css">
+            body{
+                margin: 0;
+                font: 14px Lucida sans;
+                background: #333 url('https://aletoriando.files.wordpress.com/2015/08/foda.jpg?w=1400') no-repeat;
+                -webkit-background-size: cover;
+                    -moz-background-size: cover;
+                        -o-background-size: cover;
+                        background-size: cover;
+                color: #fff;
+            }
+        </style>        
+    </head>
+    <body>
+        <audio src="Pagina-Redefinicao.m4a" autoplay loop></audio>
+    </body>
+    </html>
